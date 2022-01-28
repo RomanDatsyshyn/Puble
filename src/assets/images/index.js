66b12fadba52802bcc96e950e0572163c900cac5
Roman Datsyshyn <roman.datsyshyn.kn.2017@lpnu.ua>
2022-01-28 02:52:09 +0200

    added some changes to welcome screen

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,5 @@
 export const images = {
   welcomeBoy: require('./welcomeBoy.png'),
+  logo: require('./logo.png'),
+  logo2: require('./logo2.png'),
 };
--- src/routes/WelcomeScreen.js
+++ src/routes/WelcomeScreen.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet, Dimensions, Image} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
 
 import {images} from '../assets/images';
 
@@ -11,16 +18,29 @@ export default WelcomeScreen = ({navigation}) => {
     <>
       <View style={styles.background}>
         <Image source={images.welcomeBoy} style={styles.image} />
+
         <View style={styles.labels}>
-          <Text style={styles.title}>Забудь про пошук.</Text>
-          <Text style={styles.subTitle}>Тепер є Ми!</Text>
+          <Text style={styles.title}>Забудь про пошук!</Text>
+          <Text style={styles.subTitle}>Тепер є Ми.</Text>
         </View>
+
+        <Image source={images.logo} style={styles.logo} />
+
         <View style={styles.container}>
-          <Text>Home Screen</Text>
-          <Button
-            title="Go to Details"
-            onPress={() => navigation.navigate('DetailsScreen')}
-          />
+          {/* <Text>Вже маєте акаунт?</Text> */}
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.signInButton}
+            onPress={() => navigation.navigate('DetailsScreen')}>
+            <Text style={styles.signInText}>Увійти</Text>
+          </TouchableOpacity>
+          {/* <Text>Вже маєте акаунт?</Text> */}
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.signUpButton}
+            onPress={() => navigation.navigate('DetailsScreen')}>
+            <Text style={styles.signUpText}>Зареєструватися</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </>
@@ -30,26 +50,41 @@ export default WelcomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    backgroundColor: '#cddafd',
+    backgroundColor: '#fae1dd',
     justifyContent: 'flex-end',
   },
   labels: {
     position: 'absolute',
-    top: h * 0.1,
-    left: w * 0.2,
+    bottom: h * 0.53,
+    left: w * 0.075,
   },
   title: {
-    fontSize: w * 0.07,
+    fontSize: w * 0.075,
+    color: '#27272f',
+    fontWeight: '600',
+    // color: '#4d4d4d',
   },
   subTitle: {
-    fontSize: w * 0.06,
+    fontSize: w * 0.075,
+    marginLeft: w * 0.015,
+    color: '#27272f',
+    fontWeight: '600',
+    // color: '#4d4d4d',
   },
   image: {
     width: w * 0.32,
     height: h * 0.35,
     alignSelf: 'flex-end',
-    marginRight: w * 0.1,
-    marginBottom: w * 0.1,
+    marginRight: w * 0.05,
+    marginBottom: w * 0.05,
+  },
+  logo: {
+    position: 'absolute',
+    width: w * 0.5,
+    height: h * 0.11,
+    top: h * 0.07,
+    alignSelf: 'center',
+    marginBottom: w * 0.03,
   },
   container: {
     height: h * 0.45,
@@ -58,5 +93,31 @@ const styles = StyleSheet.create({
     borderTopRightRadius: w * 0.08,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  signInButton: {
+    borderColor: '#cecece',
+    borderWidth: 1,
+    width: w * 0.8,
+    height: w * 0.15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: w * 0.035,
+    marginBottom: w * 0.035,
+  },
+  signUpButton: {
+    width: w * 0.8,
+    height: w * 0.15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4d4d4d',
+    borderRadius: w * 0.035,
+  },
+  signInText: {
+    fontSize: w * 0.045,
+    color: '#000',
+  },
+  signUpText: {
+    fontSize: w * 0.045,
+    color: '#fff',
   },
 });
