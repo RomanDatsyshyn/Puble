e16885775bcee74f136dea5d5c982b17aba58950
Roman Datsyshyn <roman.datsyshyn.kn.2017@lpnu.ua>
2022-02-05 23:31:14 +0200

    Have added a profile tab

--- src/routes/afterAuthentication/ProfileTab/index.js
+++ src/routes/afterAuthentication/ProfileTab/index.js
@@ -8,15 +8,38 @@ import {
   Image,
 } from 'react-native';
 
-// import {icons} from '../../assets/icons';
+import {images} from '../../../assets/images';
 
 const w = Dimensions.get('window').width;
 const h = Dimensions.get('window').height;
 
-export default ProfileTab = () => {
+export default ProfileTab = ({navigation}) => {
   return (
     <View style={styles.background}>
-      <Text>ProfileTab!</Text>
+      <Image source={images.avatar} style={styles.userPhoto} />
+      <View style={styles.container}>
+        <Text style={styles.userName}>Орися</Text>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.signUpButton}
+          onPress={() => navigation.navigate('RegistrationScreen')}>
+          <Text style={styles.signUpText}>Змінити пароль</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.signUpButton}
+          onPress={() => navigation.navigate('RegistrationScreen')}>
+          <Text style={styles.signUpText}>Історія запитів</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.signInButton}
+          onPress={() => navigation.navigate('LoginScreen')}>
+          <Text style={styles.signInText}>Вийти</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -27,5 +50,55 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  userPhoto: {
+    width: w * 0.35,
+    height: w * 0.35,
+    borderRadius: w * 0.5,
+    position: 'absolute',
+    top: h * 0.02,
+    zIndex: 9,
+  },
+  userName: {
+    fontSize: w * 0.09,
+    fontWeight: '700',
+    color: '#1A374D',
+    marginBottom: w * 0.09,
+  },
+  container: {
+    width: w * 0.8,
+    height: h * 0.6,
+    borderWidth: 1,
+    borderColor: '#27272f',
+    borderRadius: w * 0.02,
+    paddingTop: w * 0.2,
+    alignItems: 'center',
+  },
+  signUpButton: {
+    width: w * 0.7,
+    height: w * 0.16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fae1dd',
+    borderRadius: w * 0.03,
+    marginBottom: w * 0.05,
+  },
+  signUpText: {
+    fontSize: w * 0.045,
+    color: '#27272f',
+  },
+  signInButton: {
+    borderColor: '#27272f',
+    borderWidth: 1,
+    width: w * 0.7,
+    height: w * 0.16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: w * 0.03,
+    marginTop: w * 0.245,
+  },
+  signInText: {
+    fontSize: w * 0.045,
+    color: '#27272f',
   },
 });
